eb742687efd0ea1aff156a831ef797db4f21ccd2
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2026-08-17 16:06:18 -0300

    feat: add design tokens, typography and base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/600.css';
+import '@fontsource/open-sans/700.css';
+import './styles/global.scss';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
